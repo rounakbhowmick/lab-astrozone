1aaf824bc021625f057d18e9f21d34c3e425d1fc
Rounak Bhowmick <rounakchakda@gmail.com>
2020-06-18 21:11:35 +0530

    Completed

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,67 @@
 package model;
 
 //Type your code here
+public class User {
+	String name;
+	String email;
+	String dob;
+	String gender;
+	String month;
+	String date;
+
+	public User(String name, String gender, String dob) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.gender = gender;
+	}
+
+//What is the date format? Please can you type a sample date? ya 18/06/2020. Good. When you have to take the month, what must you do?T first i need to import then  
+//You have taken the date as string, in such case what must you do?i need to format date, you split it first but without splitting also i can take
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		// TODO Auto-generated method stub
+		this.date = date;
+	}
+}
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,94 @@
 package service;
 
+import model.User;
+
 // Type your code
+public class AstroCalculator {
+//18/06/2020
+	public String findSign(User a) {
+		String dob = a.getDob();
+		String[] dates = dob.split("-");
+		int month = Integer.parseInt(dates[0]);
+		int date = Integer.parseInt(dates[1]);
+		System.out.println(month + " " + date);
+		if (month == 12) {
+			if (date < 22) {
+				return "Sagitarius";
+			} else {
+				return "Capricorn";
+			}
+
+		} else if (month == 01) {
+			if (date < 20) {
+				return "Capricorn";
+			} else {
+				return "Aquarious";
+			}
+
+		} else if (month == 02) {
+			if (date < 19) {
+				return "Aquarious";
+			} else {
+				return "Pisces";
+			}
+
+		}
+
+		else if (month == 03) {
+			if (date < 21) {
+				return "Pisces";
+			} else {
+				return "Aries";
+			}
+
+		}
+
+		else if (month == 04) {
+			if (date < 20) {
+				return "Aries";
+			} else {
+				return "Taurus";
+			}
+
+		} else if (month == 05) {
+			if (date < 21) {
+				return "Taurus";
+			} else {
+				return "Gemini";
+			}
+
+		} else if (month == 06) {
+			if (date < 21)
+				return "Gemini";
+			else
+				return "Cancer";
+		} else if (month == 07) {
+			if (date < 2)
+				return "Cancer";
+			else
+				return "Leo";
+		} else if (month == 8) {
+			if (date < 23)
+				return "Leo";
+			else
+				return "Virgo";
+		} else if (month == 9) {
+			if (date < 23)
+				return "Virgo";
+			else
+				return "Libra";
+		} else if (month == 10) {
+			if (date < 23)
+				return "Libra";
+			else
+				return "Scorpio";
+		} else {
+			if (date < 22)
+				return "Scorpio";
+			else
+				return "Sagitarius";
+		}
+
+	}
+
+}
